cd2058c530d61b049a9d422d16b93ae9f7462518
suvirgupta <guptasuvir@gmail.com>
2017-07-19 16:05:12 -0700

    Hive learning to work with hivecontext

--- ArunBlogP4.py
+++ ArunBlogP4.py
@@ -44,18 +44,22 @@ rdd_orders.map(lambda x: (None, x)).saveAsSequenceFile("/user/cloudera/problem5/
 rdd_orders.map(lambda x: (x[0],(x[1],x[2],x[3]))).saveAsSequenceFile("/user/cloudera/problem5/sequence_key1")
 
 ## Save the sequence file with first coloumn as key and compressed as snappy
+## below code makes and arrayWritable valuClass that is difficult to read.
 rdd_orders.map(lambda x: (x[0],(x[1],x[2],x[3]))).saveAsSequenceFile("/user/cloudera/problem5/sequence_key1-bzip", "org.apache.hadoop.io.compress.BZip2Codec")
 
 ## None key is NullWritable
 ## tuple of values is ArrayWritable
 ## save using hadoop new api
-rdd_orders.map(lambda x: (None, x)).saveAsNewAPIHadoopFile("/user/cloudera/problem5/sequence_newAPI1","org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat", keyClass = "org.apache.hadoop.io.NullWritable",valueClass = "org.apache.hadoop.io.ArrayWritable" )
-
+# dont use arrayWritable as valueClass as reading the sequential file with the arrayWritable will cause error
+## seperate class has to be written for array writable rather save you value class as string and use Text as writable
+rdd_orders.map(lambda x: (None, ''.join(str(x)))).saveAsNewAPIHadoopFile("/user/cloudera/problem5/sequence_newAPI4","org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat", keyClass = "org.apache.hadoop.io.NullWritable",valueClass = "org.apache.hadoop.io.Text" )
+rdd_seq_ord = sc.sequenceFile("/user/cloudera/problem5/sequence_newAPI4","org.apache.hadoop.io.NullWritable", 'org.apache.hadoop.io.Text')
+rdd_orders1.map(lambda x: (None, x[1])).take(10)
 ######################### Transform from parquet file################################
 
 df_orders_par = sqlc.read.parquet("/user/cloudera/problem5/parquet-snappy-compress")
 df_orders_par = sqlc.read.format("org.apache.spark.sql.parquet").load("/user/cloudera/problem5/parquet-snappy-compress")
-
+df_orders_par_h = hqlc.read.format("org.apache.spark.sql.parquet").load("/user/cloudera/problem5/parquet-snappy-compress")
 #### Save to hdfs without the compression
 sqlc.setConf("spark.sql.parquet.compression.codec", "uncompressed")
 df_orders_par.write.format("org.apache.spark.sql.parquet").save("/user/cloudera/problem5/parquet-no-compress")
@@ -68,8 +72,8 @@ df_orders_par.write.format("com.databricks.spark.avro").save("/user/cloudera/pro
 #### Transformavro snappy
 
 df_ord_av_sn = sqlc.read.format("com.databricks.spark.avro").load("/user/cloudera/problem5/avro-snappy")
-
-
+df_ord_av_sn_h = hqlc.read.format("com.databricks.spark.avro").load("/user/cloudera/problem5/avro-snappy")
+df_ord_av_sn_h.show()
 # saving directly from dataframe but cannot find any library for the compression part
 df_ord_av_sn.write.format("json").save("/user/cloudera/problem5/json-no-compress")
 df_ord_av_sn.write.json("/user/cloudera/problem5/json-no-compress2")
@@ -88,9 +92,11 @@ df_ord_json = sqlc.read.json("/user/cloudera/problem5/json-gzip1")
 df_ord_json.map(lambda x: (x[0],x[1],x[2],x[3])).saveAsTextFile("/user/cloudera/problem5/csv-gzip", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
 
-### store orc file from the sequence file
-
-rdd_seq_ord = sc.sequenceFile("/user/cloudera/problem5/sequence")
+### store orc file from the parquet file
+from pyspark.sql import HiveContext
+hqlc = HiveContext(sc)
+df_orders_par_h.write.format("orc").save("/user/cloudera/problem5/orc")
+df_orders_par_h = hqlc.read.format("org.apache.spark.sql.parquet").load("/user/cloudera/problem5/parquet-snappy-compress")
 
 
 
